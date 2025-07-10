51be94661c296281fe463f584b163a72d36ea37c
rex.c <rex.c@intteam.net>
2025-07-10 16:09:49 +0800

    practice02: hostapp read color and string value

--- app/src/main/java/com/example/hostapplication/MainActivity.kt
+++ app/src/main/java/com/example/hostapplication/MainActivity.kt
@@ -12,10 +12,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.example.hostapplication.ui.theme.HostApplicationTheme
 import com.example.aarlibrary.Echo;
+import com.example.aarlibrary.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,9 +42,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val textColor = Color(
+        ContextCompat.getColor(
+            context,
+            R.color.aarlib_black
+        )
+    )
+    val title = stringResource(id = com.example.aarlibrary.R.string.aartitle)
     Column(modifier = modifier) {
         Text(
-            text = "Hello（你好） $name!",
+            text = "Hello（你好） ${title}!",
+            color = textColor,
             modifier = modifier
         )
         Image(
