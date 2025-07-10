53635e859b9222241a5bedb4edf107c27c47a051
rex.c <rex.c@intteam.net>
2025-07-10 13:07:13 +0800

    practice00: aar專案加入圖片, hostapp顯示之

--- app/src/main/java/com/example/hostapplication/MainActivity.kt
+++ app/src/main/java/com/example/hostapplication/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hostapplication.ui.theme.HostApplicationTheme
 import com.example.aarlibrary.Echo;
@@ -18,8 +21,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val e = Echo();
-        e.sayHello();
+        Echo().sayHello();
         setContent {
             HostApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -35,10 +37,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello（你好） $name!",
-        modifier = modifier
-    )
+    Column(modifier = modifier) {
+        Text(
+            text = "Hello（你好） $name!",
+            modifier = modifier
+        )
+        Image(
+            painter = painterResource(id = com.example.aarlibrary.R.drawable.i_am_monster),
+            contentDescription = "Image from aarLibrary",
+            modifier = Modifier.fillMaxSize()
+        )
+    }
 }
 
 @Preview(showBackground = true)
