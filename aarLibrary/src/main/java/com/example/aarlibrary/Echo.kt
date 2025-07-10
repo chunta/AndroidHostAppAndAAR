ffc4ef75d5c32b7c62a982b36de6c6e5e0dbac18
rex.c <rex.c@intteam.net>
2025-07-10 12:18:00 +0800

    init host app and aar module with simple hello world

---
+++ aarLibrary/src/main/java/com/example/aarlibrary/Echo.kt
@@ -0,0 +1,7 @@
+package com.example.aarlibrary
+
+class Echo {
+    fun sayHello() {
+        println("Hello, World! From AARLibrary")
+    }
+}
---
+++ app/src/main/java/com/example/hostapplication/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.hostapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.hostapplication.ui.theme.HostApplicationTheme
+import com.example.aarlibrary.Echo;
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val e = Echo();
+        e.sayHello();
+        setContent {
+            HostApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello（你好） $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HostApplicationTheme {
+        Greeting("Android")
+    }
+}
